22dabda03584efc3577320458b7a8e8d6a09b0c5
alexey-va <freedeeml@gmail.com>
2026-09-25 21:05:06 +0300

    build: update arc-core to 2.7.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 java { toolchain { languageVersion.set(JavaLanguageVersion.of(25)) } }
 kotlin { jvmToolchain(25) }
 
-val arcCoreVersion = "2.7.11"
+val arcCoreVersion = "2.7.13"
 val worldEditVersion = "7.3.18"
 
 dependencies {
